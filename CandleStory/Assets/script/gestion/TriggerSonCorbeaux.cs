95b32b0c78833386c68110460ad767bfa548dadd
ErbakAsrevnel <jeanbaptistemaupin@gmail.com>
2020-05-06 09:58:30 +0200

    Levier+porte entrée crypte / Trigger corbeaux

---
+++ CandleStory/Assets/script/gestion/TriggerSonCorbeaux.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSonCorbeaux : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Croa Croa je suis le corbeau qui fait peur");
+        }
+    }
+}
