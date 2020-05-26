43d016df03ba2e0427a3711c9683d909c39a27d4
ErbakAsrevnel <jeanbaptistemaupin@gmail.com>
2020-05-26 12:42:40 +0200

    Petit push menu pause

---
+++ CandleStory/Assets/script/gestion/MainMenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuButton : MonoBehaviour
+{
+    public GameObject mainMenuUI;
+
+    public GameObject optionsUI;
+    public void Options()
+    {
+        mainMenuUI.SetActive(false);
+        optionsUI.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SplashScreen");
+    }
+}
---
+++ CandleStory/Assets/script/gestion/PauseButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButton : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    public GameObject optionsUI;
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameManager.GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Options()
+    {
+        pauseMenuUI.SetActive(false);
+        optionsUI.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
