28e635b46a8da0ccf694cf7a839751f47ccfa86b
ErbakAsrevnel <jeanbaptistemaupin@gmail.com>
2020-05-27 09:37:43 +0200

    Animation grille fonctionnelle

---
+++ CandleStory/Assets/script/interraction/Grille.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grille : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Update()
+    {
+        if (Lever.activatedLever == true)
+        {
+            animator.SetBool("activatedLever", true);
+        }
+    }
+}
